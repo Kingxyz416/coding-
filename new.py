f27b526f897d0be070b58ad010cdd20f5f622a43
king xyz <kingxyz416@gmail.com>
2026-05-27 10:56:29 +0530

    while

---
+++ new.py
@@ -0,0 +1,14 @@
+n= int(input("no ="))
+for i in range(1,11):
+    print(f"{n} X {i} = {n*i}")
+
+
+i=1
+while(i<11):
+    print(f"{n} X {i} = {n*i}")
+    i+=1
+
+
+
+
+
---
+++ while.py
@@ -0,0 +1,5 @@
+n = int(input("eneter no ="))
+i=0
+while(i<11):
+    print(f"{n} X {i} = {n*i}")
+    i+=1
