64c5a958ece9a67de42f3c0f5f22286fe80afd98
king xyz <kingxyz416@gmail.com>
2026-06-02 22:41:51 +0530

    pattern

--- while.py
+++ while.py
@@ -3,3 +3,12 @@ i=0
 while(i<11):
     print(f"{n} X {i} = {n*i}")
     i+=1
+for i in range(1,n+1):
+    print(" "* (n-i),end="")
+    print("*"*(2*i-1),end="")
+    print("")
+i = n
+while i > 0:
+    print(" " * (n - i) + "*" * (2 * i - 1))
+    i -= 1
+
